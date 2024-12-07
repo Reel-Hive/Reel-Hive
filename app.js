936b42bab182dba59c25a23f0525ee3f6ee1cc38
devanshi <DevanshiJodhani>
2024-12-07 18:20:36 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
+// Importing routes
+import userRoute from './routes/userRoutes.js';
+
 const app = express();
 
 // MIDDLEWARE FOR PARSE JSON
 app.use(express.json());
+app.use(express.static("public"))
+
+// MIDDLEWARE FOR COOKIES
+app.use(cookieParser());
 
 // FOR LOGGING ROUTES
 app.use(morgan('dev'));
+
+app.use('/api/v1/users', userRoute);
 
 app.use('/health', (req, res) => {
   return res.json({
