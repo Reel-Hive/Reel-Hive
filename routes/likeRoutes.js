a18b4e4ea09b6bd1234625d9259a5b7a3867343c
devanshi <DevanshiJodhani>
2025-01-08 18:19:52 +0530

    updated likeController controllers

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -3,7 +3,9 @@ import { protect } from '../middlewares/protect.js';
 import {
   getAllLikedComments,
   getAllLikedVideos,
+  toggleCommentDislike,
   toggleCommentLike,
+  toggleVideoDislike,
   toggleVideoLike,
 } from '../controllers/likeContoller.js';
 
@@ -13,7 +15,13 @@ router.use(protect);
 
 router.route('/getAllLikedVideos').get(getAllLikedVideos);
 router.route('/getAllLikedComments').get(getAllLikedComments);
-router.route('/toggle/v/:videoId').post(toggleVideoLike);
-router.route('/toggle/c/:commentId').post(toggleCommentLike);
+
+// Like and dislike video
+router.route('/toggle/v/like/:videoId').post(toggleVideoLike);
+router.route('/toggle/v/dislike/:videoId').post(toggleVideoDislike);
+
+// Like and dislike comment
+router.route('/toggle/c/like/:commentId').post(toggleCommentLike);
+router.route('/toggle/c/dislike/:commentId').post(toggleCommentDislike);
 
 export default router;
